fac3fc76d6feb0e849d2dbe3e04f4a4619fc1933
jorge jardines <jorge@themotion.com>
2017-05-03 00:16:10 +0200

    change siteurl for heroku

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://educatuperro.com'
+SITEURL = 'https://educan-website.herokuapp.com/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
