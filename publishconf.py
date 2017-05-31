a2d11053db981123f56db921c6483bb9b670f012
jorge jardines <jorge@themotion.com>
2017-05-31 23:53:47 +0200

    use subdomain

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://educan-website.herokuapp.com'
+SITEURL = 'http://www.juanmadog.com'
 RELATIVE_URLS = False
 
 # FEED_ALL_ATOM = 'feeds/all.atom.xml'
